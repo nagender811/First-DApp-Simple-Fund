9ed72e6cd925086f0a4531db3d4d7f0eeb99cc40
Nagender kumar <kumarnagender811@gmail.com>
2026-01-09 12:27:59 +0530

    Implement web3 provider loading and account retrieval in App component

--- funding/src/App.js
+++ funding/src/App.js
@@ -1,23 +1,67 @@
+import { useEffect, useState } from "react";
+import Web3 from "web3";
 
 function App() {
+  const [web3Api, setWeb3Api] = useState({ web3Api: null, provider: null });
+
+  const [account, setAccount] = useState(null);
+  useEffect(() => {
+    const loadProvider = async () => {
+      let provider = null;
+      if (window.ethereum) {
+        provider = window.ethereum;
+        try {
+          await provider.enable();
+        } catch {
+          console.error("User is not allowed");
+        }
+      } else if (window.web3) {
+        provider = window.web3.currentProvider;
+      } else if (!process.env.production) {
+        provider = new Web3.providers.HttpProvider("http://localhost:7545");
+      }
+      // console.log(window.web3);
+      // console.log(window.ethereum);
+      setWeb3Api({ web3: new Web3(provider), provider });
+    };
+    loadProvider();
+  }, []);
+
+  useEffect(() => {
+    const getAccount = async () => {
+      const accounts = await web3Api.web3.eth.getAccounts();
+      setAccount(accounts[0]);
+    };
+    web3Api.web3 && getAccount();
+  }, [web3Api.web3]);
+
+  // console.log(web3Api.web3);
+
   return (
     <>
       <div class="card text-center">
         <div class="card-header">Funding</div>
         <div class="card-body">
           <h5 class="card-title">Balance: 20 ETH </h5>
-          <p class="card-text">
-            Account :0x000000000
-          </p>
-          <button type='button' class="btn btn-success">
+          <p class="card-text">Account: {account ? account : "Not Connected"}</p>
+          {/* <button
+            type="button"
+            class="btn btn-success"
+            onClick={async () => {
+              const accounts = await window.ethereum.request({
+                method: "eth_requestAccounts",
+              });
+              console.log(accounts);
+            }}
+          >
             Connect to metamask
-          </button>
+          </button> */}
           &nbsp;
-          <button type="button" class="btn btn-success " >
+          <button type="button" class="btn btn-success ">
             Transfer
           </button>
           &nbsp;
-          <button type="button" class="btn btn-primary " >
+          <button type="button" class="btn btn-primary ">
             Withdraw
           </button>
         </div>
